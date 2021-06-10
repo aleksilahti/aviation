055e2a2848665365993cc74a3a95fdfe5e6cc956
lahtialeksi <aleksi.lahti@uros.com>
2021-06-10 16:50:13 +0300

    progress

--- aviation/src/app/app-routing.module.ts
+++ aviation/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Page components
+import { HomeComponent } from './pages/home/home.component'
+import { EchartsComponent } from './pages/echarts/echarts.component'
+import { QuizComponent } from './pages/quiz/quiz.component'
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'echarts', component: EchartsComponent },
+  { path: 'quiz', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- aviation/src/app/app.module.ts
+++ aviation/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EchartComponent } from './components/echart/echart.component';
+import { EchartsComponent } from './pages/echarts/echarts.component';
+import { QuestionComponent } from './components/question/question.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { QuizComponent } from './pages/quiz/quiz.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    EchartComponent,
+    EchartsComponent,
+    QuestionComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
